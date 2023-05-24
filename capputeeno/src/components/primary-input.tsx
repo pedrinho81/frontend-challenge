3f0ae1a4f3f47d7e8f87e1bb56407cee342083d0
Pedro Henrique <1pedrohenri@gmail.com>
2023-05-24 15:14:08 -0300

    feat: filter by search

--- capputeeno/src/components/primary-input.tsx
+++ capputeeno/src/components/primary-input.tsx
@@ -27,13 +27,16 @@ const InputContainer = styled.div`
   }
 `;
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  value: string,
+  handleChange: (value: string) => void
+}
 
 
 export function PrimaryInputWSearchIcon(props: InputProps) {
   return (
     <InputContainer>
-      <PrimaryInput {...props}/>
+      <PrimaryInput onChange={(event) => props.handleChange(event.target.value)} {...props}/>
       <SearchIcon/>
     </InputContainer>
   );
--- capputeeno/src/hooks/useProducts.tsx
+++ capputeeno/src/hooks/useProducts.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios";
 import { useFilter } from "./useFilter";
 import { mountQuery } from "@/utils/graphql-filters";
+import { useDeferredValue } from "react";
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
   return axios.post(API_URL, {query});
@@ -10,13 +11,16 @@ const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
 
 
 export function useProducts() {
-  const { type, priority } = useFilter();
+  const { type, priority, search } = useFilter();
+  const searchDeffered = useDeferredValue(search)
   const query = mountQuery(type, priority);
   const { data } = useQuery({
     queryFn: () => fetcher(query),
     queryKey: ["products", type, priority],
   });
+  const products = data?.data?.data?.allProducts
+  const filteredProducts = products?.filter(product => product.name.toLocaleLowerCase().includes(searchDeffered.toLocaleLowerCase()))
   return {
-    data: data?.data?.data?.allProducts,
+    data: filteredProducts,
   };
 }
