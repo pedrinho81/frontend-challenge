829abfa94960bc232b0264f44663a70ff65336d2
Pedro Henrique <1pedrohenri@gmail.com>
2023-05-24 12:19:23 -0300

    feat: consume products database

---
+++ capputeeno/src/components/products-list.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useProducts } from "@/hooks/useProducts"
+
+interface ProductsListProps {
+}
+
+export function ProductsList(props:ProductsListProps) {
+  const {data} = useProducts()
+  console.log(data)
+  return (
+    <></>
+  )
+}
