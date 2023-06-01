27f14a108b67d5d162b9d16b81059ec78a32cfa0
Pedro Henrique <1pedrohenri@gmail.com>
2023-06-01 12:27:17 -0300

    refactor: resume order

--- capputeeno/src/app/cart/page.tsx
+++ capputeeno/src/app/cart/page.tsx
@@ -2,8 +2,8 @@
 
 import { BackBtn } from "@/components/back-button";
 import { CartItem } from "@/components/cart/cart-item";
+import { CartResume } from "@/components/cart/cart-resume";
 import { DefaultPageLayout } from "@/components/default-page-layout";
-import { Divider } from "@/components/divider";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { ProductInCart } from "@/types/product";
 import { formatPrice } from "@/utils/formatPrice";
@@ -52,64 +52,7 @@ const CartList = styled.ul`
   margin-top: 24px;
 `;
 
-const CartResultContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-  min-width: 22rem;
-  max-height: 43.75rem;
-  padding: 16px 24px;
-  background: white;
-  div {
-    width: 100%;
-  }
 
-  h3 {
-    font-weight: 600;
-    font-size: 20px;
-    color: var(--text-dark-2);
-    text-transform: uppercase;
-    margin-bottom: 30px;
-  }
-  ul {
-    color: var(--text-dark);
-    text-decoration: underline;
-    text-transform: uppercase;
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 150%;
-    cursor: pointer;
-    li ~ li {
-      margin-top: 12px;
-    }
-  }
-`;
-
-const TotalItem = styled.div<{ isBold?: boolean }>`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-
-  font-weight: ${(props) => (props.isBold ? "600" : "400")};
-  font-size: 16px;
-  line-height: 150%;
-
-  margin-bottom: 12px;
-`;
-
-const ShopBtn = styled.button`
-  color: white;
-  border-radius: 4px;
-  background: var(--success-color);
-  padding: 12px;
-  width: 100%;
-  border: none;
-  margin-top: 40px;
-  cursor: pointer;
-  font-weight: 500;
-`;
 
 export default function CartPage() {
   const { value, updateLocalStorage } = useLocalStorage<ProductInCart[]>(
@@ -123,13 +66,8 @@ export default function CartPage() {
       0
     );
   };
-
   const cartTotal = formatPrice(calculateTotal(value));
 
-  const deliveryFee = calculateTotal(value) > 90000 ? 0 : 4000;
-  const cartTotalWithDelivery = formatPrice(
-    calculateTotal(value) + deliveryFee
-  );
 
   const handleUpdateQuantity = (id: string, quantity: number) => {
     const newValue = value.map((item) => {
@@ -167,33 +105,7 @@ export default function CartPage() {
             ))}
           </CartList>
         </CartListContainer>
-        <CartResultContainer>
-          <div>
-            <h3>Resumo do Pedido</h3>
-            <TotalItem>
-              <p>Subtotal de produtos</p>
-              <p>{cartTotal}</p>
-            </TotalItem>
-            <TotalItem>
-              <p>Entrega</p>
-              <p>{formatPrice(deliveryFee)}</p>
-            </TotalItem>
-            <Divider />
-            <TotalItem isBold>
-              <p>Total</p>
-              <p>{cartTotalWithDelivery}</p>
-            </TotalItem>
-            <ShopBtn>FINALIZAR COMPRA</ShopBtn>
-          </div>
-          <div>
-            <ul>
-              <li>Ajuda</li>
-              <li>Reembolso</li>
-              <li>Entregas e Frete</li>
-              <li>Trocas e devoluções</li>
-            </ul>
-          </div>
-        </CartResultContainer>
+        <CartResume calculateTotal={calculateTotal(value)}/> 
       </Container>
     </DefaultPageLayout>
   );
---
+++ capputeeno/src/components/cart/cart-resume.tsx
@@ -0,0 +1,105 @@
+"use client";
+import { Divider } from "@/components/divider";
+import { styled } from "styled-components";
+import { formatPrice } from "@/utils/formatPrice";
+import { ProductInCart } from "@/types/product";
+interface CartResumeProps {
+  calculateTotal: number;
+}
+const CartResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  min-width: 22rem;
+  max-height: 43.75rem;
+  padding: 16px 24px;
+  background: white;
+  div {
+    width: 100%;
+  }
+
+  h3 {
+    font-weight: 600;
+    font-size: 20px;
+    color: var(--text-dark-2);
+    text-transform: uppercase;
+    margin-bottom: 30px;
+  }
+  ul {
+    color: var(--text-dark);
+    text-decoration: underline;
+    text-transform: uppercase;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 150%;
+    cursor: pointer;
+    li ~ li {
+      margin-top: 12px;
+    }
+  }
+`;
+
+const TotalItem = styled.div<{ isBold?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  font-weight: ${(props) => (props.isBold ? "600" : "400")};
+  font-size: 16px;
+  line-height: 150%;
+
+  margin-bottom: 12px;
+`;
+
+const ShopBtn = styled.button`
+  color: white;
+  border-radius: 4px;
+  background: var(--success-color);
+  padding: 12px;
+  width: 100%;
+  border: none;
+  margin-top: 40px;
+  cursor: pointer;
+  font-weight: 500;
+`;
+export function CartResume({ calculateTotal }: CartResumeProps) {
+
+
+
+  const deliveryFee = calculateTotal > 90000 ? 0 : 4000;
+  const cartTotalWithDelivery = formatPrice(
+    calculateTotal + deliveryFee
+  );
+
+  return (
+    <CartResultContainer>
+      <div>
+        <h3>Resumo do Pedido</h3>
+        <TotalItem>
+          <p>Subtotal de produtos</p>
+          <p>{formatPrice(calculateTotal)}</p>
+        </TotalItem>
+        <TotalItem>
+          <p>Entrega</p>
+          <p>{formatPrice(deliveryFee)}</p>
+        </TotalItem>
+        <Divider />
+        <TotalItem isBold>
+          <p>Total</p>
+          <p>{cartTotalWithDelivery}</p>
+        </TotalItem>
+        <ShopBtn>FINALIZAR COMPRA</ShopBtn>
+      </div>
+      <div>
+        <ul>
+          <li>Ajuda</li>
+          <li>Reembolso</li>
+          <li>Entregas e Frete</li>
+          <li>Trocas e devoluções</li>
+        </ul>
+      </div>
+    </CartResultContainer>
+  );
+}
