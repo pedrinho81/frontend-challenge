c04f60e9e1a7e5d750f24e7206c5e44b68884023
Pedro Henrique <1pedrohenri@gmail.com>
2023-06-02 14:38:03 -0300

    fix: render msg if hasn't product

--- capputeeno/src/components/products-list.tsx
+++ capputeeno/src/components/products-list.tsx
@@ -5,18 +5,29 @@ import { ProductCard } from "./product-card";
 import { styled } from "styled-components";
 
 const ListContainer = styled.div`
+  position: relative;
   display: grid;
   grid-template-columns: repeat(auto-fill, 256px);
   grid-gap: 2rem;
   width: 100%;
   margin-top: 2rem;
+  h1 {
+    grid-column-start: 1;
+    font-size: 14px;
+    @media (min-width: ${props => props.theme.tabletBreakpoint}) {
+      grid-column-start: 2;
+      font-size: 24px;
+    }
+    color: var(--orange-low);
+    text-align: center;
+  }
 `;
 export function ProductsList() {
   const { data } = useProducts();
-  console.log(data)
+
   return (
     <ListContainer>
-      {data?.map((product) => (
+      {data && data.length > 0 ? data.map((product) => (
         <ProductCard
           key={product.id}
           image={product.image_url}
@@ -24,7 +35,9 @@ export function ProductsList() {
           title={product.name}
           id={product.id}
         />
-      ))}
+      )) : (
+        <h1>Em breve mais opções...</h1>
+      )}
     </ListContainer>
   );
 }
